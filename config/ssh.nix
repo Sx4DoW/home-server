e8e69b9569b0469fcd4ea310e722ebc101f55b90
Sx4DoW <yuformini9@gmail.com>
2026-02-09 14:54:28 +0100

    refactor: clean up SSH authorized keys formatting

--- config/ssh.nix
+++ config/ssh.nix
@@ -23,8 +23,8 @@
   '';
 
   users.users.void.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILgB+hEPpEMjBFSTrqGET5UH4CYAMksr9sH8mJ1vSntD yuformini9@gmail.com" # Laptop
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgLDVhkPrdPrFjjfi+DDojhHH4K91naRO1fCo6oEKhL yuformini9@gmail.com" # Desktop
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKTxhPKn0TPhtawVsLOCsW09qWpXo4LOjHdcaRmTmxf yuformini9@gmail.com" # Phone
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILgB+hEPpEMjBFSTrqGET5UH4CYAMksr9sH8mJ1vSntD Laptop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgLDVhkPrdPrFjjfi+DDojhHH4K91naRO1fCo6oEKhL Desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKTxhPKn0TPhtawVsLOCsW09qWpXo4LOjHdcaRmTmxf Phone"
   ];
 }
