161ea021ff940b230823621b62d49649272ef762
Sx4DoW <yuformini9@gmail.com>
2026-02-09 10:30:19 +0100

    added git submodule for docker folder and created new ssh key for containers repo and updated ssh.nix to use both keys

--- config/ssh.nix
+++ config/ssh.nix
@@ -10,6 +10,18 @@
     };
   };
 
+  programs.ssh.extraConfig = ''
+    # Primary key for the NixOS config repo
+    Host github.com
+        HostName github.com
+        IdentityFile ~/.ssh/id_nixos
+
+    # Secondary key for the Docker containers submodule
+    Host github-containers
+        HostName github.com
+        IdentityFile ~/.ssh/id_containers
+  '';
+
   users.users.void.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILgB+hEPpEMjBFSTrqGET5UH4CYAMksr9sH8mJ1vSntD yuformini9@gmail.com" # Laptop
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgLDVhkPrdPrFjjfi+DDojhHH4K91naRO1fCo6oEKhL yuformini9@gmail.com" # Desktop
