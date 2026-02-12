e6720134e05216d2c9620329a5b9d5df87f544cc
Sx4DoW <yuformini9@gmail.com>
2026-02-12 23:49:45 +0100

    update rebuild script to pull submodules and merge changes

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "--- Pulling latest changes from GitHub ---"
-cd "$CONFIG_DIR" && git pull origin main
+cd "$CONFIG_DIR" && git pull --recurse-submodules && git submodule update --remote --merge
 
 echo "--- Running NixOS Rebuild ---"
 sudo nixos-rebuild switch -I nixos-config="$CONFIG_DIR/config/configuration.nix"
