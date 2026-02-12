99a66f7ad5c90e3b32e8605639e80a931cfec2ac
Sx4DoW <yuformini9@gmail.com>
2026-02-12 12:57:47 +0100

    refactor: simplify void-sync alias for git operations

--- config/environment.nix
+++ config/environment.nix
@@ -40,6 +40,6 @@
     dlog = "docker logs -f";
 
     # Git sync
-    void-sync = "cd ~/home-server && git pull && git submodule update --remote --merge && cd docker && git checkout main && git pull && cd ..";
+    void-sync = "cd ~/home-server && git pull --recurse-submodules && git submodule update --remote --merge";
   };
 }
